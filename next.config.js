25c3dd4fe6798e66348192e775cdc6c1fe0ff020
g.n <41154336+gnaraine@users.noreply.github.com>
2022-11-07 13:20:13 -0500

    init

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-
 // next.config.js
 const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  assetPrefix: isProd ? "/gnaraine.github.io/" : "",
-  images: {
-    domains: ["images.ctfassets.net"],
-  },
-  compiler: {
-    styledComponents: true,
-  }
+export const assetPrefix = isProd ? "/gnaraine.github.io/" : "";
+export const images = {
+  domains: ["images.ctfassets.net"],
+};
+export const compiler = {
+  styledComponents: true,
 };
